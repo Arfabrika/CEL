08253591bc2a1e38f088107f8d8daf5df5b188b1
Arfabrika <73736515+Arfabrika@users.noreply.github.com>
2020-11-02 10:17:07 +0300

    First project version
    Added project base

---
+++ CEL/main.cpp
@@ -0,0 +1,20 @@
+/* FILE NAME: T35GLOBE.C
+ * PROGRAMMER: FABRIKA ARTEM (AF5)
+ * DATE: 05.01.2019
+ * PERPOSE: WiNAPI windowed application sample
+ */
+
+#include "main_header.h"
+
+/* Main program function 
+   ARGUMENTS:
+     None;
+   RETURNS:
+      - 1 if all okey, 0 if error.*/
+int main( void )
+{
+    printf("Hello world");
+    return 1;
+} /* End of 'main' function */
+
+/* END OF 'main.cpp' FILE */
---
+++ CEL/main_header.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+#include <windows.h>
+
+/* END OF 'main_header.h' FILE */
